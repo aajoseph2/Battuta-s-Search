849d1796112d065d19022920cb877910163c98f1
aajoseph2 <89800875+aajoseph2@users.noreply.github.com>
2023-09-07 21:38:28 -0700

    removed javadoc warnings and traces

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -11,7 +11,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.Normalizer;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Pattern;
 
@@ -30,186 +29,170 @@ public class Driver {
 	 * inverted index.
 	 *
 	 * @param args flag/value pairs used to start this program
-	 * @throws IOException 
+	 * @throws IOException
 	 * 
 	 */
-	
-	public static TreeMap<Path,Integer> fileInfo = new TreeMap<>();
+
+	public static TreeMap<Path, Integer> fileInfo = new TreeMap<>();
 	public static final Pattern SPLIT_REGEX = Pattern.compile("(?U)\\p{Space}+");
 	public static final Pattern CLEAN_REGEX = Pattern.compile("(?U)[^\\p{Alpha}\\p{Space}]+");
-	
+
 	public static String clean(String text) {
 		String cleaned = Normalizer.normalize(text, Normalizer.Form.NFD);
 		cleaned = CLEAN_REGEX.matcher(cleaned).replaceAll("");
 		return cleaned.toLowerCase();
 	}
-	
+
 	public static String[] split(String text) {
 		return text.isBlank() ? new String[0] : SPLIT_REGEX.split(text.strip());
 	}
-	
+
 	public static String[] parse(String text) {
 		return split(clean(text));
 	}
-	
 
 	public static void iterDirectory(Path input) {
-		
-	    try (DirectoryStream<Path> stream = Files.newDirectoryStream(input)) {
-	        for (Path entry : stream) {
-	            if (entry.getFileName().toString().equals(".DS_Store")) {
-	                continue;  
-	            }
-	            if (Files.isDirectory(entry)) {
-	                iterDirectory(entry);
-	            } else {
-	            	String fileNameLower = entry.getFileName().toString().toLowerCase();
-	            	if(fileNameLower.endsWith(".txt") || fileNameLower.endsWith(".text")) {
-	                    try {
-	                        textProcess(entry);
-	                    } catch (MalformedInputException e) {
-	                        System.out.println("Skipped due to encoding issues: " + entry);
-	                    } catch (IOException e) {
-	                        e.printStackTrace();
-	                    }
-	                }
-	            }
-	        }
-	    } catch (IOException e) {
-	    	System.out.println("File was not able to be read!");
-	        e.printStackTrace();
-	    }
+
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(input)) {
+			for (Path entry : stream) {
+				if (entry.getFileName().toString().equals(".DS_Store")) {
+					continue;
+				}
+				if (Files.isDirectory(entry)) {
+					iterDirectory(entry);
+				} else {
+					String fileNameLower = entry.getFileName().toString().toLowerCase();
+					if (fileNameLower.endsWith(".txt") || fileNameLower.endsWith(".text")) {
+						try {
+							textProcess(entry);
+						} catch (MalformedInputException e) {
+							System.out.println("Skipped due to encoding issues: " + entry);
+						} catch (IOException e) {
+							System.out.println("An error occurred while iterating the directory: " + input.toString());
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("File was not able to be read!");
+
+		}
 	}
 
-
-	
-	
 	public static void textProcess(Path input) throws IOException {
-		
-		if(!Files.exists(input)) {
-	        System.out.println("Invalid file: " + input.toString());
-	        return;
-	    }
-		
-		StringBuilder inputText = new StringBuilder();
-		 try (BufferedReader reader = Files.newBufferedReader(input, UTF_8)) {
-			 String line;
-		        while ((line = reader.readLine()) != null) {
-		            inputText.append(line).append("\n");
-		        }
-		    }
-				
 
-		String[] str = parse(inputText.toString());	
-		
+		if (!Files.exists(input)) {
+			System.out.println("Invalid file: " + input.toString());
+			return;
+		}
+
+		StringBuilder inputText = new StringBuilder();
+		try (BufferedReader reader = Files.newBufferedReader(input, UTF_8)) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				inputText.append(line).append("\n");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred while reading the file: " + input.toString());
+		}
+
+		String[] str = parse(inputText.toString());
+
 		if (str.length != 0) {
 			fileInfo.put(input, str.length);
 		}
-		
-		
+
 	}
-	
+
 	public static Path toAbsolutePath(Path path, String relativePathString) {
-	    if (!path.isAbsolute()) {
-	        Path currentWorkingDir = Paths.get("").toAbsolutePath();
-	        path = Paths.get(currentWorkingDir.toString(), relativePathString);
-	    }
-	    return path;
-	}
-	
-	
-	
-	public static String mapToJson() {       
-	    StringBuilder json = new StringBuilder("{\n");
-	    
-
-	    for (var entry : fileInfo.entrySet()) {
-	        json.append("  \"")
-	            .append(entry.getKey())
-	            .append("\": ")
-	            .append(entry.getValue())
-	            .append(",\n");
-	    }       
-
-	    if (json.length() > 2) {
-	        json.setLength(json.length() - 2);
-	    } else {
-	        return "{\n}";
-	    }
-	    
-	    json.append("\n}");
-	    return json.toString();
-	}
-
-	
-	public static void writeJsonToFile(String json, Path outputPath) {
-        try {
-        	System.out.println(outputPath);
-            Files.write(outputPath, json.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-	
-	
-	
-	public static void main(String[] args) throws IOException {
-		HashMap<String,String> flags = new HashMap<>();
-		int bound = args.length;
-		
-		
-		for (int i = 0; i < args.length; i++) {
-	        if (args[i].startsWith("-")) {	        	        	                	
-	        	if ( (i+1 < bound) && (!args[i+1].startsWith("-"))) {
-	        		flags.put(args[i], args[i+1]);
-	        		i++;
-	        	}else {
-	        		flags.put(args[i], "default");
-	        	}
-
-	        }
+		if (!path.isAbsolute()) {
+			Path currentWorkingDir = Paths.get("").toAbsolutePath();
+			path = Paths.get(currentWorkingDir.toString(), relativePathString);
 		}
-	        
-	        for (var commands : flags.entrySet()) {
-	        	String flag = commands.getKey();
-	        	String path = commands.getValue();
-	        	
-	        	switch (flag) {	        	
-		        	case "-text" :
-		        		fileInfo.clear();
-		        		if (path.equals("default")) {
-		        			System.out.println("Missing file path to read!\n");
-		        			continue;
-		        		} else {
-		        			Path content = Paths.get(path);
-		        			if (Files.isDirectory(content)) {
-			                    iterDirectory(content);
-			                } else {
-			                	textProcess(content);
-			                }
-		        		}
-		        		break;
-		        		
-		        	case "-counts" :
-		        		if (path.equals("default")) {
-		        			Path countPath = Paths.get("counts.json");
-			                writeJsonToFile(mapToJson(), countPath);
-		        		} else {
-		        			Path countPath = Paths.get(path);
-		 	                writeJsonToFile(mapToJson(), countPath);		 	                
-		        		}
-		        		break;
-		        		
-		        	//case "-index" :
-		        	//	System.out.println("Pending");
-		        	
-		        	default :
-		        		System.out.println("Ignoring unknown argument: " + flag);
-		                break;
-		        }
-	        }
-	        
-	    
+		return path;
+	}
+
+	public static String mapToJson() {
+		StringBuilder json = new StringBuilder("{\n");
+
+		for (var entry : fileInfo.entrySet()) {
+			json.append("  \"").append(entry.getKey()).append("\": ").append(entry.getValue()).append(",\n");
+		}
+
+		if (json.length() > 2) {
+			json.setLength(json.length() - 2);
+		} else {
+			return "{\n}";
+		}
+
+		json.append("\n}");
+		return json.toString();
+	}
+
+	public static void writeJsonToFile(String json, Path outputPath) {
+		try {
+			System.out.println(outputPath);
+			Files.write(outputPath, json.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		HashMap<String, String> flags = new HashMap<>();
+		int bound = args.length;
+
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].startsWith("-")) {
+				if ((i + 1 < bound) && (!args[i + 1].startsWith("-"))) {
+					flags.put(args[i], args[i + 1]);
+					i++;
+				} else {
+					flags.put(args[i], "default");
+				}
+
+			}
+		}
+
+		for (var commands : flags.entrySet()) {
+			String flag = commands.getKey();
+			String path = commands.getValue();
+
+			switch (flag) {
+			case "-text":
+				fileInfo.clear();
+				if (path.equals("default")) {
+					System.out.println("Missing file path to read!\n");
+					continue;
+				} else {
+					Path content = Paths.get(path);
+					if (Files.isDirectory(content)) {
+						iterDirectory(content);
+					} else {
+						textProcess(content);
+					}
+				}
+				break;
+
+			case "-counts":
+				if (path.equals("default")) {
+					Path countPath = Paths.get("counts.json");
+					writeJsonToFile(mapToJson(), countPath);
+				} else {
+					Path countPath = Paths.get(path);
+					writeJsonToFile(mapToJson(), countPath);
+				}
+				break;
+
+			// case "-index" :
+			// System.out.println("Pending");
+
+			default:
+				System.out.println("Ignoring unknown argument: " + flag);
+				break;
+			}
+		}
+
 	}
 
 }
