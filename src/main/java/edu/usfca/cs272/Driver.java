a15424a75cf41e5d61bf69c23304aa88ffc45ed6
aajoseph2 <89800875+aajoseph2@users.noreply.github.com>
2023-09-12 14:07:34 -0700

    back to v1.0.8 code for main

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -1,33 +1,18 @@
 package edu.usfca.cs272;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.nio.charset.MalformedInputException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.Normalizer;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Pattern;
-
-import opennlp.tools.stemmer.Stemmer;
-import opennlp.tools.stemmer.snowball.SnowballStemmer;
-import opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM;
 
 /**
  * Class responsible for running this project based on the provided command-line
@@ -46,18 +31,6 @@ public class Driver {
 
 	public static TreeMap<Path, Integer> fileInfo = new TreeMap<>();
 	/**
-	 * Map of file: position
-	 */
-	public static TreeMap<String, List<Integer>> nestMap = new TreeMap<>();
-	/**
-	 * map of stem:nestMap
-	 */
-	public static Map<String, TreeMap<String, List<Integer>>> invertMap = new HashMap<>();
-	/**
-	 * map to which will be written to json, index
-	 */
-	public static TreeMap<String, String> formatMap = new TreeMap<>();
-	/**
 	 * Text pattern to follow
 	 */
 	public static final Pattern SPLIT_REGEX = Pattern.compile("(?U)\\p{Space}+");
@@ -65,217 +38,6 @@ public class Driver {
 	 * Text pattern to follow
 	 */
 	public static final Pattern CLEAN_REGEX = Pattern.compile("(?U)[^\\p{Alpha}\\p{Space}]+");
-
-
-	/**
-	 * Indents the writer by the specified number of times. Does nothing if the
-	 * indentation level is 0 or less.
-	 *
-	 * @param writer the writer to use
-	 * @param indent the number of times to indent
-	 * @throws IOException if an IO error occurs
-	 */
-	public static void writeIndent(Writer writer, int indent) throws IOException {
-		while (indent-- > 0) {
-			writer.write("  ");
-		}
-	}
-
-	/**
-	 * Indents and then writes the String element.
-	 *
-	 * @param element the element to write
-	 * @param writer the writer to use
-	 * @param indent the number of times to indent
-	 * @throws IOException if an IO error occurs
-	 */
-	public static void writeIndent(String element, Writer writer, int indent) throws IOException {
-		writeIndent(writer, indent);
-		writer.write(element);
-	}
-
-	/**
-	 * Indents and then writes the text element surrounded by {@code " "} quotation
-	 * marks.
-	 *
-	 * @param element the element to write
-	 * @param writer the writer to use
-	 * @param indent the number of times to indent
-	 * @throws IOException if an IO error occurs
-	 */
-	public static void writeQuote(String element, Writer writer, int indent) throws IOException {
-		writeIndent(writer, indent);
-		writer.write('"');
-		writer.write(element);
-		writer.write('"');
-	}
-
-	/**
-	 * Writes the elements as a pretty JSON array.
-	 *
-	 * @param elements the elements to write
-	 * @param writer the writer to use
-	 * @param indent the initial indent level; the first bracket is not indented,
-	 *   inner elements are indented by one, and the last bracket is indented at the
-	 *   initial indentation level
-	 * @throws IOException if an IO error occurs
-	 *
-	 * @see Writer#write(String)
-	 * @see #writeIndent(Writer, int)
-	 * @see #writeIndent(String, Writer, int)
-	 */
-	public static void writeArray(Collection<? extends Number> elements, Writer writer, int indent) throws IOException {
-
-		writer.write("[\n");
-
-	    Iterator<? extends Number> iterator = elements.iterator();
-	    while (iterator.hasNext()) {
-	        Number element = iterator.next();
-
-	        writeIndent(writer, indent + 1);
-	        writer.write(element.toString());
-
-	        if (iterator.hasNext()) {
-	            writer.write(",");
-	        }
-	        writer.write("\n");
-	    }
-
-	    writeIndent(writer, indent);
-	    writer.write("]");
-
-
-	}
-
-
-
-	/**
-	 * Writes the elements as a pretty JSON array to file.
-	 *
-	 * @param elements the elements to write
-	 * @param path the file path to use
-	 * @throws IOException if an IO error occurs
-	 *
-	 * @see Files#newBufferedReader(Path, java.nio.charset.Charset)
-	 * @see StandardCharsets#UTF_8
-	 * @see #writeArray(Collection, Writer, int)
-	 */
-	public static void writeArray(Collection<? extends Number> elements, Path path) throws IOException {
-		try (BufferedWriter writer = Files.newBufferedWriter(path, UTF_8)) {
-			writeArray(elements, writer, 0);
-		}
-	}
-
-	/**
-	 * Returns the elements as a pretty JSON array.
-	 *
-	 * @param elements the elements to use
-	 * @return a {@link String} containing the elements in pretty JSON format
-	 *
-	 * @see StringWriter
-	 * @see #writeArray(Collection, Writer, int)
-	 */
-	public static String writeArray(Collection<? extends Number> elements) {
-		try {
-			StringWriter writer = new StringWriter();
-			writeArray(elements, writer, 0);
-			return writer.toString();
-		}
-		catch (IOException e) {
-			return null;
-		}
-	}
-
-	/**
-	 * Writes the elements as a pretty JSON object with nested arrays. The generic
-	 * notation used allows this method to be used for any type of map with any type
-	 * of nested collection of number objects.
-	 *
-	 * @param elements the elements to write
-	 * @param writer the writer to use
-	 * @param indent the initial indent level; the first bracket is not indented,
-	 *   inner elements are indented by one, and the last bracket is indented at the
-	 *   initial indentation level
-	 * @return String
-	 * @throws IOException if an IO error occurs
-	 *
-	 * @see Writer#write(String)
-	 * @see #writeIndent(Writer, int)
-	 * @see #writeIndent(String, Writer, int)
-	 * @see #writeArray(Collection)
-	 */
-	public static String writeObjectArrays(Map<String, ? extends Collection<? extends Number>> elements, Writer writer, int indent) throws IOException {
-	    writer.write("{\n");
-
-	    var iterator = elements.entrySet().iterator();
-
-	    while (iterator.hasNext()) {
-	        Map.Entry<String, ? extends Collection<? extends Number>> entry = iterator.next();
-
-	        String elementString = entry.getKey();
-	        Collection<? extends Number> elementCollection = entry.getValue();
-
-	        writer.write("  ");
-	        writeQuote(elementString, writer, indent + 1);
-	        writer.write(": ");
-
-	        if (elementCollection != null && !elementCollection.isEmpty()) {
-	            writeArray(elementCollection, writer, indent + 2);
-	        } else {
-	            writer.write("[\n");
-	            writeIndent(writer, indent + 1);
-	            writer.write("]");
-	        }
-
-	        if (iterator.hasNext()) {
-	            writer.write(",");
-	        }
-	        writer.write("\n");
-	    }
-
-	    writeIndent(writer, indent);
-	    //writer.write("}");
-
-	    return writer.toString();
-	}
-
-
-	/**
-	 * Writes the elements as a pretty JSON object with nested arrays to file.
-	 *
-	 * @param elements the elements to write
-	 * @param path the file path to use
-	 * @throws IOException if an IO error occurs
-	 *
-	 * @see Files#newBufferedReader(Path, java.nio.charset.Charset)
-	 * @see StandardCharsets#UTF_8
-	 * @see #writeObjectArrays(Map, Writer, int)
-	 */
-	public static void writeObjectArrays(Map<String, ? extends Collection<? extends Number>> elements, Path path) throws IOException {
-		try (BufferedWriter writer = Files.newBufferedWriter(path, UTF_8)) {
-			writeObjectArrays(elements, writer, 0);
-		}
-	}
-
-	/**
-	 * Returns the elements as a pretty JSON object with nested arrays.
-	 *
-	 * @param elements the elements to use
-	 * @return a {@link String} containing the elements in pretty JSON format
-	 *
-	 * @see StringWriter
-	 * @see #writeObjectArrays(Map, Writer, int)
-	 */
-	public static String writeObjectArrays(Map<String, ? extends Collection<? extends Number>> elements) {
-		try {
-			StringWriter writer = new StringWriter();
-			writeObjectArrays(elements, writer, 0);
-			return writer.toString();
-		}
-		catch (IOException e) {
-			return null;
-		}
-	}
 
 	/**
 	 * @param text text to be parsed
@@ -304,44 +66,6 @@ public class Driver {
 	}
 
 	/**
-	 * Parses the line into a list of cleaned and stemmed words.
-	 *
-	 * @param line the line of words to clean, split, and stem
-	 * @param stemmer the stemmer to use
-	 * @return a list of cleaned and stemmed words in parsed order
-	 *
-	 * @see #parse(String)
-	 * @see Stemmer#stem(CharSequence)
-	 */
-	public static ArrayList<String> listStems(String line, Stemmer stemmer) {
-		ArrayList<String> stemList = new ArrayList<>();
-		String[] words = parse(line);
-		for (int i = 0; i < words.length; i++) {
-	        stemList.add(stemmer.stem(words[i]).toString());
-	    }
-
-		return stemList;
-
-	}
-
-	/**
-	 * Parses the line into a list of cleaned and stemmed words using the default
-	 * stemmer for English.
-	 *
-	 * @param line the line of words to parse and stem
-	 * @return a list of cleaned and stemmed words in parsed order
-	 *
-	 * @see SnowballStemmer#SnowballStemmer(ALGORITHM)
-	 * @see ALGORITHM#ENGLISH
-	 * @see #listStems(String, Stemmer)
-	 */
-	public static ArrayList<String> listStems(String line) {
-		Stemmer stem = new SnowballStemmer(ENGLISH);
-		return listStems(line, stem);
-
-	}
-
-	/**
 	 * @param input the directory that recurses on its self until it reaches a base text file
 	 */
 	public static void iterDirectory(Path input) {
@@ -353,12 +77,10 @@ public class Driver {
 				}
 				if (Files.isDirectory(entry)) {
 					iterDirectory(entry);
-					System.out.println("Directory: " + entry);
 				} else {
 					String fileNameLower = entry.getFileName().toString().toLowerCase();
 					if (fileNameLower.endsWith(".txt") || fileNameLower.endsWith(".text")) {
 						try {
-							System.out.println("File: " + entry);
 							textProcess(entry);
 						} catch (MalformedInputException e) {
 							System.out.println("Skipped due to encoding issues: " + entry);
@@ -385,87 +107,22 @@ public class Driver {
 			return;
 		}
 
-		StringBuilder countText = new StringBuilder();
+		StringBuilder inputText = new StringBuilder();
 		try (BufferedReader reader = Files.newBufferedReader(input, UTF_8)) {
 			String line;
-			int pos = 1;
 			while ((line = reader.readLine()) != null) {
-
-				countText.append(line).append("\n");
-				ArrayList<String> stems = listStems(line);
-				for (String stem: stems) {
-					processIndex(stem, input.toString(), pos);
-					pos++;
-				}
+				inputText.append(line).append("\n");
 			}
 		} catch (IOException e) {
 			System.out.println("An error occurred while reading the file: " + input.toString());
 		}
 
-		String[] contents = parse(countText.toString());
+		String[] str = parse(inputText.toString());
 
-		if (contents.length != 0) {
-			fileInfo.put(input, contents.length);
+		if (str.length != 0) {
+			fileInfo.put(input, str.length);
 		}
 
-
-	}
-
-	/**
-	 * @param stem stem to be added in  invertMap
-	 * @param fn file name
-	 * @param num position in file
-	 */
-	public static void processIndex(String stem, String fn, Integer num) {
-
-		nestMap = invertMap.getOrDefault(stem, new TreeMap<>());
-		List<Integer> positionsList = nestMap.getOrDefault(fn, new ArrayList<>());
-		positionsList.add(num);
-
-		nestMap.put(fn, positionsList);
-		invertMap.put(stem, nestMap);
-
-
-		formatMap.put(stem, writeObjectArrays(nestMap));
-
-
-}
-
-	/**
-	 * @return formatMap to json string
-	 */
-	public static String finalIndexJson() {
-		StringWriter buffer = new StringWriter();
-
-		var iterator = Driver.formatMap.entrySet().iterator();
-		buffer.write("{\n");
-
-
-		while (iterator.hasNext()) {
-    	var entry = iterator.next();
-
-        String stem = entry.getKey();
-        String loc = entry.getValue();
-
-        buffer.write("  ");
-        buffer.write('"');
-        buffer.write(stem);
-        buffer.write("\": ");
-        buffer.write(loc.toString());
-
-
-        if (iterator.hasNext()) {
-          buffer.write("  },\n");
-      } else {
-          buffer.write("  }\n");
-      }
-    }
-
-
-    buffer.write("}");
-    formatMap.clear();
-
-    return buffer.toString();
 	}
 
 
@@ -507,10 +164,10 @@ public class Driver {
 	 * @throws IOException In case file cannot be read
 	 */
 	public static void main(String[] args) throws IOException {
-		LinkedHashMap<String, String> flags = new LinkedHashMap<>();
+		HashMap<String, String> flags = new HashMap<>();
 		int bound = args.length;
 
-		for (int i = 0; i < bound; i++) {
+		for (int i = 0; i < args.length; i++) {
 			if (args[i].startsWith("-")) {
 				if ((i + 1 < bound) && (!args[i + 1].startsWith("-"))) {
 					flags.put(args[i], args[i + 1]);
@@ -526,14 +183,9 @@ public class Driver {
 			String flag = commands.getKey();
 			String path = commands.getValue();
 
-
 			switch (flag) {
-
 			case "-text":
-				invertMap.clear();
-				nestMap.clear();
 				fileInfo.clear();
-				formatMap.clear();
 				if (path.equals("default")) {
 					System.out.println("Missing file path to read!\n");
 					continue;
@@ -557,29 +209,16 @@ public class Driver {
 				}
 				break;
 
-			 case "-index" :
-				 if (path.equals("default")) {
-						Path indexPath = Paths.get("index.json");
-						String indexJson = finalIndexJson();
-						writeJsonToFile(indexJson, indexPath);
-				} else {
-						Path indexPath = Paths.get(path);
-						String indexJson = finalIndexJson();
-						writeJsonToFile(indexJson, indexPath);
-					}
-					break;
+			// case "-index" :
+			// System.out.println("Pending");
 
 			default:
 				System.out.println("Ignoring unknown argument: " + flag);
 				break;
 			}
 		}
-		System.out.println(invertMap);
-		System.out.println("\n");
-		System.out.println(formatMap);
-
 
 	}
-	//u need to fix how the maps are being created
-	//fix the wrte array so that u dont call it so much.
+
 }
+
