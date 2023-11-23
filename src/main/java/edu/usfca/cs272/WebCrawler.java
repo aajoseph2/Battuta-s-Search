d423b63e3b5d08761a782ff5a35fdb06dfa279af
aajoseph2 <89800875+aajoseph2@users.noreply.github.com>
2023-11-23 08:21:11 -0800

    passing tests, but html cleaner most likely failing

--- src/main/java/edu/usfca/cs272/WebCrawler.java
+++ src/main/java/edu/usfca/cs272/WebCrawler.java
@@ -1,14 +1,20 @@
 package edu.usfca.cs272;
 
+import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
+
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.util.HashSet;
-import java.util.Set;;
+import java.util.Set;
+
+import opennlp.tools.stemmer.Stemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
+
 
 /**
- * Class to iterate through a list of links or link. Fetches and
- * cleans html data, then processes stemmed words into the inverted
- * index data strcuture.
+ * Class to iterate through a list of links or link. Fetches and cleans html
+ * data, then processes stemmed words into the inverted index data strcuture.
  */
 public class WebCrawler {
 	/**
@@ -22,7 +28,7 @@ public class WebCrawler {
 
 	/**
 	 * @param index Inverted Index of which contains all the word data, passed
-	 * through in constuctor
+	 *   through in constuctor
 	 */
 	public WebCrawler(InvertedIndex index) {
 		this.index = index;
@@ -44,7 +50,8 @@ public class WebCrawler {
 		String html = HtmlFetcher.fetch(url, 3);
 		if (html != null) {
 			String cleanHtml = HtmlCleaner.stripHtml(html);
-			processText(cleanHtml, url.toString());
+			URI baseLocation = LinkFinder.cleanUri(LinkFinder.makeUri(url.toString()));
+			processText(cleanHtml, baseLocation.toString());
 		}
 
 		// also track the searched links
@@ -57,6 +64,13 @@ public class WebCrawler {
 	 * @param location Location or link of the text
 	 */
 	private void processText(String text, String location) {
-		//todo
+		int pos = 1;
+		Stemmer stemmer = new SnowballStemmer(ENGLISH);
+		String[] words = TextParser.parse(text);
+
+		for (String word : words) {
+			index.addData(stemmer.stem(word).toString(), location, pos);
+			pos++;
+		}
 	}
 }
