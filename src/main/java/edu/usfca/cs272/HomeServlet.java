2abd303c567d6069da3913f2532b3203481e4c09
aajoseph2 <89800875+aajoseph2@users.noreply.github.com>
2023-12-11 19:16:54 -0800

    cleaning up

--- src/main/java/edu/usfca/cs272/HomeServlet.java
+++ src/main/java/edu/usfca/cs272/HomeServlet.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class HomeServlet extends HttpServlet {
 	/** Class version for serialization, in [YEAR][TERM] format (unused). */
-	private static final long serialVersionUID = 202308;
+	private static final long serialVersionUID = 1;
 
 	/** The title to use for this webpage. */
 	private static final String title = "Battuta's";
